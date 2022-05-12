574a17bf9b650854e45b303edf34077d4a92790e
skylar <46612603+skywur@users.noreply.github.com>
2022-05-12 00:16:24 -0500

    Add files via upload

---
+++ gameindex.js
@@ -0,0 +1,6 @@
+$.getJSON(
+    "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=97246405904689D0C42E9FCD79023EFA&steamids=76561198198543162/",
+    function(data) {
+        console.log(data);
+    }
+);
