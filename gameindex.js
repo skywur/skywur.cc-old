5ded04b6d64d5c5d71052d1c2048276a9d1ec7c6
skylar <46612603+skywur@users.noreply.github.com>
2022-05-12 06:07:41 -0500

    Delete gameindex.js

--- gameindex.js
+++
@@ -1,6 +0,0 @@
-$.getJSON(
-    "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=97246405904689D0C42E9FCD79023EFA&steamids=76561198198543162/",
-    function(data) {
-        console.log(data);
-    }
-);
