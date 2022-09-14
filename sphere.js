4d96038e3b795249cd7119b2273cf8030e97f3ca
skylar <46612603+skywur@users.noreply.github.com>
2022-09-14 18:45:42 -0500

    Add files via upload

--- sphere.js
+++ sphere.js
@@ -1,47 +1,84 @@
-/* global THREE */
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
 
-window.addEventListener('load', init)
-let scene
-let camera
-let renderer
-let mesh
+/*
+ In addition to creating the renderer instance, we also need to set the size at which we want it to render our app.
+ It's a good idea to use the width and height of the area we want to fill with our game
+ - in this case, the width and height of the browser window. For performance intensive games, you can also give setSize smaller values,
+ like window.innerWidth/2 and window.innerHeight/2, for half the resolution.
+ This does not mean that the game will only fill half the window, but rather look a bit blurry and scaled up.
 
-function init() {
-    scene = new THREE.Scene()
+ Last but not least, we add the renderer element to our HTML document.
+ This is a <canvas> element the renderer uses to display the scene to us.
+*/
 
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    camera.position.z = 40
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-    renderer = new THREE.WebGLRenderer()
-    renderer.setSize(window.innerWidth, window.innerHeight)
 
-    scene.add(new THREE.AmbientLight(0x404040))
 
-    const loader = new THREE.OBJLoader()
-    loader.load('discord.obj',
-        (obj) => {
-            let material = new THREE.PointsMaterial({ color: 0xFFFFFF, size: 0.25 })
-            mesh = new THREE.Points(obj.children[0].geometry, material)
-            mesh.position.y = -15
-            scene.add(mesh)
+/* Create Lights: PointLight / SpotLight etc.*/
+var spotLight = new THREE.SpotLight(0xffffff);
+spotLight.position.set(100, 100, 100);
+spotLight.castShadow = true; //If set to true light will cast dynamic shadows. Warning: This is expensive and requires tweaking to get shadows looking right.
+spotLight.shadowMapWidth = 1024;
+spotLight.shadowMapHeight = 1024;
+spotLight.shadowCameraNear = 500;
+spotLight.shadowCameraFar = 4000;
+spotLight.shadowCameraFov = 30;
+scene.add(spotLight);
 
-        },
-        (xhr) => {
-            console.log(xhr)
-        },
-        (err) => {
-            console.error("loading .obj went wrong, ", err)
-        }
-    )
-
-    document.body.appendChild(renderer.domElement)
-    animationLoop()
+/* Create Material */
+function Mat() {
+    var material = new THREE.MeshPhongMaterial({
+        color: new THREE.Color("rgb(70, 0, 88)"), //Diffuse color of the material
+        emissive: new THREE.Color("rgb(165, 64, 255)"), //Emissive(light) color of the material, essentially a solid color unaffected by other lighting. Default is black.
+        specular: new THREE.Color("rgb(255, 161, 0)"),
+        /*Specular color of the material, i.e., how shiny the material is and the color of its shine.
+                                                              Setting this the same color as the diffuse value (times some intensity) makes the material more metallic-looking;
+                                                              setting this to some gray makes the material look more plastic. Default is dark gray.*/
+        shininess: 1, //How shiny the specular highlight is; a higher value gives a sharper highlight. Default is 30.
+        shading: THREE.SmoothtShading, //How the triangles of a curved surface are rendered: THREE.SmoothShading, THREE.FlatShading, THREE.NoShading
+        wireframe: 1, //THREE.Math.randInt(0,1)
+        transparent: 1,
+        opacity: 0.15 //THREE.Math.randFloat(0,1)
+    });
+    return material;
 }
 
-function animationLoop() {
-    renderer.render(scene, camera)
-    if (mesh) {
-        mesh.rotation.y += 0.005
-    }
-    requestAnimationFrame(animationLoop)
+/* Create Geometry */
+var geometry = new THREE.SphereGeometry(50, 20, 20, 0, Math.PI * 2, 0, Math.PI);
+//SphereGeometry(radius, widthSegments, heightSegments, phiStart, phiLength, thetaStart, thetaLength)
+
+/* Create Earth Sphere*/
+var earth = new THREE.Mesh(geometry, Mat());
+
+/*
+var numSphere = 30;
+var tabSphere = [];
+for(var i=0: i<numSphere; i++){
+  tabShpere.push(new Sphere(i));
+  scene.add(tabSphere[i].b);
 }
+*/
+
+scene.add(earth);
+
+camera.position.z = 90;
+
+
+
+/*
+  This will create a loop that causes the renderer to draw the scene 60 times per second.
+  If you're new to writing games in the browser, you might say "why don't we just create a setInterval?
+  The thing is - we could, but requestAnimationFrame has a number of advantages.
+  Perhaps the most important one is that it pauses when the user navigates to another browser tab, hence not wasting their precious processing power and battery life.
+*/
+function render() {
+    requestAnimationFrame(render);
+    earth.rotation.x += 0.005;
+    earth.rotation.y += 0.005;
+    renderer.render(scene, camera);
+}
+render();
